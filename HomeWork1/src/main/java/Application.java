740016679f5ee7fbb131b95011233b2f78f807bd
Grigori Uleev <uleev777@yandex.ru>
2021-04-23 14:18:53 +0400

    homework1

---
+++ HomeWork1/src/main/java/Application.java
@@ -0,0 +1,21 @@
+import model.Kotik;
+
+public class Application {
+    public static void main(String[] args) {
+
+        Kotik kotik = new Kotik(5, "Барсик", 5, "Мяу-мяу");
+        Kotik.count++;
+
+        Kotik kotik1 = new Kotik();
+        Kotik.count++;
+        kotik1.setPrettiness(3);
+        kotik1.setName("Васька");
+        kotik1.setWeight(7);
+        kotik1.setMeow("Мяу-мяу-мяу");
+        kotik1.liveAnotherDay();
+
+        System.out.println("Котика зовут " + kotik1.getName() + ", его вес " + kotik1.getWeight());
+        System.out.println(kotik.getMeow().equals(kotik1.getMeow()));
+        System.out.println("Количество котиков: " + Kotik.count);
+    }
+}
